e74a8c487adb95330e14a6ebaa8c872d1bb41ff9
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-04-28 09:57:33 +0200

    multer directory bugfix

--- api/middleware/FileManager.js
+++ api/middleware/FileManager.js
@@ -26,12 +26,15 @@ const filter = (req, file, cb) => {
     // to indicate if the file should be accepted
 
     //console.log("filter", storageFolder+"/"+file['originalname'])
+    let path = req.url.replace(process.env["API_BASE_URL"], '/').replace('//','/')
 
     try {
-        if( fs.statSync(storageFolder+"/"+file['originalname'])) { // will return null is reading performed
+        if( fs.statSync(storageFolder+path+file['originalname'])) { // will return null is reading performed
+            console.log("file already exists :",storageFolder+req.path+file['originalname'])
             cb(null, false)
         }
     } catch (e) { // in case of error that's mean that incoming file can be written on disk
+        console.log("file write :",storageFolder+req.path+file['originalname'])
         cb(null, true)
     }
     // You can always pass an error if something goes wrong:
